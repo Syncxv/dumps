acfef98cfecb850b35fa9d3219d7598c8cf7ff5b
Syncx <47534062+Syncxv@users.noreply.github.com>
2021-02-26 16:06:40 +0530

    Update eval.js

--- eval.js
+++ eval.js
@@ -64,66 +64,66 @@ const {
     Button
   } = require("powercord/components");
   
-  class SettingsWOAyeyye extends React.Component {
-    render() {
-      const {
-        getSetting,
-        toggleSetting,
-        updateSetting
-      } = this.props;
-      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(TextInput, {
-        note: "Text to replace your token with",
-        defaultValue: getSetting("tokenReplacer", "[REDACTED]"),
-        onChange: val => updateSetting("tokenReplacer", val)
-      }, "Token Replacer"), /*#__PURE__*/React.createElement(TextInput, {
-        note: "Custom formatting! Check out the github repo for more info.",
-        defaultValue: getSetting("evalFormat", "⏱️ Took {time}{n}🔍 Typeof {type}{n}{output}"),
-        onChange: val => updateSetting("evalFormat", val)
-      }, "Output Formatting"), /*#__PURE__*/React.createElement(TextInput, {
-        note: "Amount of messages to cache for auto completion",
-        defaultValue: getSetting("autoCompleteAmount", 25),
-        onChange: val => updateSetting("autoCompleteAmount", isNaN(val) ? 25 : Number(val)),
-        disabled: !getSetting("autoCompleteToggle")
-      }, "Auto Complete"), /*#__PURE__*/React.createElement(SwitchItem, {
-        note: "Turns auto complete on or off.",
-        value: getSetting("autoCompleteToggle", true),
-        onChange: () => toggleSetting("autoCompleteToggle")
-      }, "Auto Complete"), /*#__PURE__*/React.createElement(SwitchItem, {
-        note: "For the users who are inexpirenced. I'm not sure why you'd want this, but it's here just in case.",
-        value: getSetting("safeEval", false),
-        onChange: () => toggleSetting("safeEval")
-      }, "Basic/Safe Evaluate"), /*#__PURE__*/React.createElement(ButtonItem, {
-        note: "Clear all of the auto complete cache, just in case.",
-        disabled: !getSetting("autoCompleteToggle"),
-        button: "Clear Data",
-        color: Button.Colors.RED,
-        onClick: () => powercord.api.commands.commands["eval"].messages = []
-      }, "Clear Auto Complete Cache"));
-    }
+//   class SettingsWOAyeyye extends React.Component {
+//     render() {
+//       const {
+//         getSetting,
+//         toggleSetting,
+//         updateSetting
+//       } = this.props;
+//       return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(TextInput, {
+//         note: "Text to replace your token with",
+//         defaultValue: getSetting("tokenReplacer", "[REDACTED]"),
+//         onChange: val => updateSetting("tokenReplacer", val)
+//       }, "Token Replacer"), /*#__PURE__*/React.createElement(TextInput, {
+//         note: "Custom formatting! Check out the github repo for more info.",
+//         defaultValue: getSetting("evalFormat", "⏱️ Took {time}{n}🔍 Typeof {type}{n}{output}"),
+//         onChange: val => updateSetting("evalFormat", val)
+//       }, "Output Formatting"), /*#__PURE__*/React.createElement(TextInput, {
+//         note: "Amount of messages to cache for auto completion",
+//         defaultValue: getSetting("autoCompleteAmount", 25),
+//         onChange: val => updateSetting("autoCompleteAmount", isNaN(val) ? 25 : Number(val)),
+//         disabled: !getSetting("autoCompleteToggle")
+//       }, "Auto Complete"), /*#__PURE__*/React.createElement(SwitchItem, {
+//         note: "Turns auto complete on or off.",
+//         value: getSetting("autoCompleteToggle", true),
+//         onChange: () => toggleSetting("autoCompleteToggle")
+//       }, "Auto Complete"), /*#__PURE__*/React.createElement(SwitchItem, {
+//         note: "For the users who are inexpirenced. I'm not sure why you'd want this, but it's here just in case.",
+//         value: getSetting("safeEval", false),
+//         onChange: () => toggleSetting("safeEval")
+//       }, "Basic/Safe Evaluate"), /*#__PURE__*/React.createElement(ButtonItem, {
+//         note: "Clear all of the auto complete cache, just in case.",
+//         disabled: !getSetting("autoCompleteToggle"),
+//         button: "Clear Data",
+//         color: Button.Colors.RED,
+//         onClick: () => powercord.api.commands.commands["eval"].messages = []
+//       }, "Clear Auto Complete Cache"));
+//     }
   
-  }
+//   }
   
-  ;
+//   ;
 class EvalCommand extends Plugin {
     startPlugin() {
       // All of the settings. Looks ugly but idk how else to make this look "pretty"
-      const replace = this.settings.get("tokenReplacer", "[REDACTED]");
-      const format = this.settings.get(
-        "evalFormat",
-        "⏱️ Took {time}{n}🔍 Typeof {type}{n}{output}"
-      );
-      const safeEval = this.settings.get("safeEval", false);
+//       const replace = this.settings.get("tokenReplacer", "[REDACTED]");
+//       const format = this.settings.get(
+//         "evalFormat",
+//         "⏱️ Took {time}{n}🔍 Typeof {type}{n}{output}"
+//       );
+//       const safeEval = this.settings.get("safeEval", false);
       //const autoCompleteAmount = this.settings.get("autoCompleteAmount", 25);
       const autoCompleteAmount = 25
       //const allowAutoComplete = this.settings.get("autoCompleteToggle", true);
       const allowAutoComplete = true;
   
       // Register Eval settings
-      window.settings.registerSettings("eval-plugin", {
-        category: this.entityID,
-        label: "Eval Plugin",
-        render: SettingsWOAyeyye,
-      });
+//       window.settings.registerSettings("eval-plugin", {
+//         category: this.entityID,
+//         label: "Eval Plugin",
+//         render: SettingsWOAyeyye,
+//       });
   
       // Eval command, of course
       window.commandsclass.registerCommand({
