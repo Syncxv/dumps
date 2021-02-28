3b9e5a99be6f5949d41aeab679756c36df9e19b9
Syncx <47534062+Syncxv@users.noreply.github.com>
2021-03-01 02:49:53 +0530

    Create openspotify.js

---
+++ openspotify.js
@@ -0,0 +1,43 @@
+var { Plugin } = require('powercord/entities');
+var { inject, uninject } = require('powercord/injector');
+var { getModule } = require('powercord/webpack');
+
+var MessageContent = getModule(
+    m => m.type && m.type.displayName == 'MessageContent',
+    false
+);
+
+class OpenInSpotify extends Plugin {
+    async startPlugin() {
+        // prettier-ignore
+        inject('open-in-spotify', MessageContent, 'type', this.transformSpotifyLink);
+        MessageContent.type.displayName = 'MessageContent';
+    }
+
+    transformSpotifyLink(e, res) {
+        const children = res.props.children.find(c => Array.isArray(c));
+
+        if (children) {
+            for (var i = 0; i < children.length; i++) {
+                // prettier-ignore
+                if (!children[i].props?.href?.toLowerCase().includes('open.spotify.com')) continue;
+
+                const url = children[i].props.href.split('/');
+
+                if (!url[3] || !url[4]) continue;
+                // prettier-ignore
+                if (!['embed', 'search', 'local', 'playlist', 'user', 'starred', 'artist', 'album', 'track', 'episode'].includes(url[3].toLowerCase())) continue;
+
+                children[i].props.href = `spotify:${url[3]}:${url[4]}`;
+            }
+        }
+
+        return res;
+    }
+
+    pluginWillUnload() {
+        uninject('open-in-spotify');
+    }
+};
+var openspotuf = new OpenInSpotify
+openspotuf.startPlugin()
